523a7b4a0d54531c5d763c32f340eef502cafb6d
JamesLilliott <jameslilliott1997@gmail.com>
2023-05-09 22:53:47 +0100

    Remove overrides

--- src/JamesLogger.php
+++ src/JamesLogger.php
@@ -8,48 +8,8 @@ use Psr\Log\AbstractLogger;
 
 class JamesLogger extends AbstractLogger
 {
-    public function __construct(private WriterInterface $writer)
+    public function __construct(private readonly WriterInterface $writer)
     {}
-
-    public function emergency(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[EMERGENCY] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function alert(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[ALERT] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function critical(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[CRITICAL] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function error(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[ERROR] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function warning(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[WARNING] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function notice(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[NOTICE] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function info(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[INFO] ' . $message . ' ' . json_encode($context));
-    }
-
-    public function debug(\Stringable|string $message, array $context = []): void
-    {
-        $this->writer->write('[DEBUG] ' . $message . ' ' . json_encode($context));
-    }
 
     public function log($level, \Stringable|string $message, array $context = []): void
     {
